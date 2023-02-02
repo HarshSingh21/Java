7ae8d8be72dea2e85b18a9846c9249532dadf2b8
HarshSingh21 <singhharsh8283@gmail.com>
2023-02-02 09:58:09 +0530

    hello java

---
+++ Basic/hello.java
@@ -0,0 +1,32 @@
+package Basic;
+class hello
+ {     
+    public static void main(String a [] ) 
+
+{     
+      int harsh = 23;
+      int harsh1 = 24;
+      int result = harsh+ harsh1;
+      float  hi = 6.6f;
+      double he = 6.32;
+      char b = 'h';
+      b++;  // to increment the value of letter h .
+
+      byte c = 127;
+      c++;
+        int d = c;
+        System.out.println(d);
+             System.out.println(hi);
+
+             System.out.println(he);
+
+             System.out.println("harsh");
+
+             System.out.println(3+6);
+
+             System.out.println(result);
+
+             System.out.println(b);
+
+}
+    }
