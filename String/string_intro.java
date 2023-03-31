5978e7a6b253411ac00a000208a2d25850641d0b
Harsh <singhharsh8283@gmail.com>
2023-04-01 00:01:09 +0530

    string_intro

--- String/string_intro.java
+++ String/string_intro.java
@@ -2,6 +2,7 @@ package String;
 
 public class string_intro {
     public static void main(String[] args) {
+//string intro and its working in java all in this file
         System.out.println("Hello we will learn string ");
     }
 }
